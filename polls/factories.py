49ab31cb8f7207b3f9d0b8d96081c43b1143690d
ddkangfu <ddkangfu@gmail.com>
2014-12-30 21:53:50 +0800

    增加了简单的示例for fuzzy生成随机字符串的功能。

--- polls/factories.py
+++ polls/factories.py
@@ -1,4 +1,5 @@
 from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyText
 
 from django.utils import timezone
 
@@ -14,6 +15,12 @@ class QuestionFactory(DjangoModelFactory):
         """
         #django_get_or_create = ('question_text',)
 
-    question_text = "How do you do ?"
+    #question_text = "How do you do ?"
+    # Use Fuzzy to generate random question text
+    # you can use FuzzyAttribute, FuzzyText, FuzzyChoice, FuzzyInteger
+    # FuzzyDecimal, FuzzyFloat, FuzzyDate, FuzzyDateTime, FuzzyNaiveDateTime
+    # or Custom fuzzy fields to generate randome field data.
+    # Reference: http://factoryboy.readthedocs.org/en/latest/fuzzy.html
+    question_text = FuzzyText(length=20, prefix='Q:', suffix='?')
     pub_date = timezone.now()
 
